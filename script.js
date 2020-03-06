8d52640f3d9b8b7ecdab10770f258f4ba5f980f7
Spiky <ryan.j@epoxstudios.com>
2020-03-06 14:35:42 -0500

    Started a big re-write, keeping old script as reference until done

--- script.js
+++ script.js
@@ -1,3 +1,95 @@
+var wordIndex;
+
+// Initialize everything
+function Initialize() {
+    // Give all buttons the functions needed
+    const letters = document.querySelectorAll('button');
+    letters.forEach(button => {
+        button.addEventListener('click', () => {
+            // Check if button has been chosen
+            if (button.classList.contains('chosen')) { return; }
+            // Add the classlist
+            button.classList.add('chosen');
+            // Check the letter
+            CheckLeter(button.innerHTML.toString());
+        })
+    })
+    // Reset word index
+    wordIndex = -1;
+    // Reset the displayed word
+    myWordDisplayed.innerHTML = '';
+    // call function to get a new word and display it
+    GetNewWord();
+}
+
+// Get a new word
+async function GetNewWord() {
+    // Setup request
+    const jsonData = { inforequest : "GetNewWord" };
+    const information = {
+        method : 'POST',
+        headers : { 'Content-Type': 'application/json' },
+        body : JSON.stringify(jsonData)
+    };
+    // Send out information
+    const response = await fetch('/hangmanapi', information);
+    // Wait for the response
+    const data = await response.json();
+    // Setup word to display
+    DisplayWord(data.length);
+    // Save index
+    wordIndex = data.index;
+}
+
+// Display the word with the given length
+function DisplayWord(wordLength) {
+    let myWord = '';
+    // Loop through
+    for (i = 1; i < wordLength; i++) {
+        myWord += '_ ';
+    }
+    // One last one without the additional space at the end
+    myWord += '_';
+    // Set the html doc to reflect the string
+    document.getElementById('displayedWord').innerHTML = myWord;
+}
+
+// Check the letter with the given letter
+async function CheckLetter(letter) {
+    // Setup request
+    const jsonData = { inforequest : "CheckLetter", letter: letter, index: wordIndex };
+    const information = {
+        method : 'POST',
+        headers : { 'Content-Type': 'application/json' },
+        body : JSON.stringify(jsonData)
+    };
+    const response = await fetch('/hangmanapi', information);
+    const data = await response.json();
+    // Parse out the data
+    if (data.array.length == 0) {
+        // We did not find a letter in the word
+        UpdateImage();
+    } else {
+        // Update the word with that letter
+    }
+    // Check if we have lost the game or not
+
+}
+
+// Updates the image for hangman
+function UpdateImage() {
+    // Get the current turn number by the image itself
+    let turnNumber = 0; // Assume 0 as default
+    turnNumber = parseInt(document.getElementById('hangman').src.split("images/")[1].charAt(0));
+    // Update the number and then update the image
+    document.getElementById('hangman').src = "images/" + (turnNumber + 1).toString() + ".svg";
+}
+
+
+
+
+
+/*
 var myWord = getWord();
 var myWordDisplayed = document.getElementById('displayedWord');
 var hangmanImage = document.getElementById('hangman');
@@ -74,3 +166,4 @@ function updateImage() {
 function getWord() {
     return 'Hello';
 }
+*/
