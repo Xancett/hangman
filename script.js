640e162e40ff6b81aac79287cd2b3117a25f4082
Spiky <ryan.j@epoxstudios.com>
2020-03-07 17:40:56 -0500

    Another fix attempt

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ function UpdateWord(array, letter) {
     let myWord = document.getElementById('displayedWord').innerHTML.split(" ");
     let displayedWord = '';
     for (i = 0; i < myWord.length; i++) {
-        if (array.hasOwnProperty(i)) {
+        if (Object.values(array).includes(i)) {
             displayedWord += letter + ' ';
         } else {
             displayedWord += myWord[i] + ' ';
