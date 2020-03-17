4fd9dde949aa0923ba3559ae3bb755668cc84992
Spiky <ryan.j@epoxstudios.com>
2020-03-17 13:17:13 -0400

    Created initial template

---
+++ stats/script.js
@@ -0,0 +1,15 @@
+// Start off by grabbing information
+(function () {
+    GetData(UpdateStats);
+})()
+
+// Gets the data from the server
+async function GetData(callback) {
+    // Callback when data is ready
+    // callback(datatosend);
+}
+
+// Updates the information displayed in the table and pie chart
+function UpdateStats(data) {
+
+}
