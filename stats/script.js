9a1130984303b524cf776c3bd4306fec86ce83d4
Xancett <ryan.j@epoxstudios.com>
2020-04-05 08:18:18 -0400

    Set win percentage based on the data

--- stats/script.js
+++ stats/script.js
@@ -57,4 +57,9 @@ function UpdateStats(data) {
             }
         }
     }
+    // Set the win percentage
+    let winPercent = (data.Wins / (data.Wins + data.Losses)) * 100;
+    winPercent = parseInt(winPercent.toString());
+    document.getElementById('winpercent').innerHTML = "Win percentage: " + winPercent + "%";
+    // Send that percentage to css to draw the pie accordingly
 }
